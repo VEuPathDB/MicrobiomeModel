5d834726eae2538f7a97bd7fc193550cd89193e1
Wojtek Bazant <wbazant@vet.upenn.edu>
2020-04-28 12:07:06 -0400

    BIOM files more changes
    The dataset installer:
    - updated to the "1.0, 2.0, or 2.1" version
    - does not pass the summary to the script: we instead make a string summary in the installer
    
    Queries:
    - Remove the unnecessary ProfileSet
    - Presenter query
    - Rename a param biom_datasets -> biom_dataset, there's just one in the dropdown
    
    Join on the user_dataset_id in the Characteristics query to help the optimiser - does not stop a full table scan but stuff gets slightly reordered

--- Model/src/main/java/org/apidb/apicommon/model/userdataset/BiomTypeHandler.java
+++ Model/src/main/java/org/apidb/apicommon/model/userdataset/BiomTypeHandler.java
@@ -19,7 +19,11 @@ import org.gusdb.wdk.model.user.dataset.UserDatasetTypeHandler;
 public class BiomTypeHandler extends UserDatasetTypeHandler {
 
   private final static String NAME = "BIOM";
-  private final static String VERSION = "1.0";
+  /* Can't actually handle all of those BIOM files
+  *  Only does 1.0 sparse files, with data points in the tsv file
+  *  This was done because there is no good way to add dependencies to the installer script
+  */
+  private final static String VERSION = "1.0, 2.0, or 2.1";
   private final static String DISPLAY = "BIOM";
 
   @Override
@@ -35,25 +39,22 @@ public class BiomTypeHandler extends UserDatasetTypeHandler {
   public String getDisplay() {
         return DISPLAY;
   }
-
   @Override
   public String[] getInstallInAppDbCommand(UserDataset userDataset, Map<String, Path> fileNameToTempFileMap, String projectId) {
-      Path biomFile = fileNameToTempFileMap.get("data.biom");
-      if (biomFile == null) {
-	  throw new RuntimeException("failed to get data.biom");
+      Path metadataJson = fileNameToTempFileMap.get("metadata.json");
+      if (metadataJson == null) {
+        throw new RuntimeException("Failed to get: metadata.json");
       }
-      /*
-       Wojtek: This is a hack. We want to show the summary on sample page, but I see no convenient way to query from the users database there
-       Instead, pass the summary through, and store it in the ud_ProfileSet.name field
-      */
-      String summary;
-      try {
-          summary = userDataset.getMeta().getSummary();
-      }
-      catch(WdkModelException e) {
-          throw new RuntimeException("Error getting dataset summary: " + e.toString());
-      }
-      String[] cmd = {"installBiomUserDataset", userDataset.getUserDatasetId().toString(), biomFile.toString(), projectId, summary};
+      Path dataTsv = fileNameToTempFileMap.get("data.tsv");
+      String dataTsvOrEmptyStringMeaningThingsToTheInstaller = dataTsv == null ? "" : dataTsv.toString();
+
+      String[] cmd = {
+          "installBiomUserDataset",
+          userDataset.getUserDatasetId().toString(),
+          metadataJson.toString(),
+          dataTsvOrEmptyStringMeaningThingsToTheInstaller,
+          projectId
+      };
       return cmd;
   }
 
